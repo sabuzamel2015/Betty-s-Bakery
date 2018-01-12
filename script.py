9f369359217996bb8b6d3afbd24c366b513747cc
sabuzamel2015 <33916928+sabuzamel2015@users.noreply.github.com>
2018-01-11 23:33:24 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,24 @@
 import numpy as np
 
+#Created an array
 cupcakes = np.array([2, 0.75, 2, 1, 0.5])
 
+#Generated the CSV file
 recipes = np.genfromtxt('recipes.csv', delimiter = ',')
 
+#This is used to find the number of eggs used in each recipe
 eggs = recipes[:,2]
 
+#Logical statement used to find all recipes that require one egg 
 eggs ==1
 
+#This is used to find the recipes to bake the cookies. Yum!
 cookies = recipes[2,:]
 
+#Number of each recipe needed in order to make 2 batches of cupcakes
 double_batch = cupcakes*2
 
+#Adding the cookies and double batch of cupcakes in order to create a grocery_list 
 grocery_list = cookies + double_batch
 
 print(grocery_list)   
